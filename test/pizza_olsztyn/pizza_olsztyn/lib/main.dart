f419de257c53682e3f95e9bf6414cf21180371d0
macieksulikowski <suleymaniana99@gmail.com>
2022-12-14 13:18:54 +0100

    Organize files

--- test/pizza_olsztyn/pizza_olsztyn/lib/main.dart
+++ test/pizza_olsztyn/pizza_olsztyn/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pizza_olsztyn/app/home/home_page.dart';
+import 'package:pizza_olsztyn/app/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -38,17 +40,12 @@ class RootPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return const Scaffold(
-              body: Center(
-                child: Text("Jestes niezalogowany"),
-              ),
-            );
+            return LoginPage();
           }
-          return Scaffold(
-            body: Center(
-              child: Text("Jestes zalogowany jako ${user.email}"),
-            ),
-          );
+          return HomePage(user: user);
         });
   }
 }
+
+
+
