408ef998cdf1e339d0b76b661a457bbd87856db2
macieksulikowski <suleymaniana99@gmail.com>
2022-12-15 10:20:35 +0100

    add error handling

--- test/pizza_olsztyn/pizza_olsztyn/lib/app/login/login_page.dart
+++ test/pizza_olsztyn/pizza_olsztyn/lib/app/login/login_page.dart
@@ -2,13 +2,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({
     Key? key,
   }) : super(key: key);
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +28,28 @@ class LoginPage extends StatelessWidget {
             children: [
               const Text("Zaloguj sie"),
               TextField(
-                controller: emailController,
+                controller: widget.emailController,
                 decoration: const InputDecoration(hintText: "Email"),
               ),
               TextField(
-                controller: passwordController,
+                controller: widget.passwordController,
                 decoration: const InputDecoration(hintText: "Hasło"),
                 obscureText: true,
               ),
+              const SizedBox(height: 20),
+              Text(errorMessage),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
                   try {
                     await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text,
+                      email: widget.emailController.text,
+                      password: widget.passwordController.text,
                     );
                   } catch (error) {
+                    setState(() {
+                      errorMessage = error.toString();
+                    });
                     print(error);
                   }
                 },
