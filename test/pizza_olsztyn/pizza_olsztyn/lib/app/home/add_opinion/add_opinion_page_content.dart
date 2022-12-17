03cc348bdc36ebf635627f49e016bd6aeb88b580
macieksulikowski <suleymaniana99@gmail.com>
2022-12-17 18:53:38 +0100

    Implement adding restaurant

--- test/pizza_olsztyn/pizza_olsztyn/lib/app/home/add_opinion/add_opinion_page_content.dart
+++ test/pizza_olsztyn/pizza_olsztyn/lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var pizzaName = '';
+
+  @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Dwa'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Podaj nazwe restauracji",
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Podaj nazwe pizzy",
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                pizzaName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'pizza': pizzaName,
+                'rating': 3.0,
+              });
+            },
+            child: Text("Dodaj"),
+          ),
+        ],
+      ),
     );
   }
 }
