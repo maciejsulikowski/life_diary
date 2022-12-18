81070b2750b76429b301714af6d75b20165609bc
macieksulikowski <suleymaniana99@gmail.com>
2022-12-18 13:22:46 +0100

    Change bottom navigation bar onSave index = 0

--- test/pizza_olsztyn/pizza_olsztyn/lib/app/home/add_opinion/add_opinion_page_content.dart
+++ test/pizza_olsztyn/pizza_olsztyn/lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -4,8 +4,11 @@ import 'package:flutter/material.dart';
 
 class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
+    required this.onSave,
     Key? key,
   }) : super(key: key);
+
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -19,52 +22,58 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Podaj nazwe restauracji",
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Podaj nazwe restauracji",
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
             ),
-            onChanged: (newValue) {
-              setState(() {
-                restaurantName = newValue;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Podaj nazwe pizzy",
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Podaj nazwe pizzy",
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  pizzaName = newValue;
+                });
+              },
             ),
-            onChanged: (newValue) {
-              setState(() {
-                pizzaName = newValue;
-              });
-            },
-          ),
-          Slider(
-            onChanged: (newValue) {
-              setState(() {
-                rating = newValue;
-              });
-            },
-            value: rating,
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: rating.toString(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore.instance.collection('restaurants').add({
-                'name': restaurantName,
-                'pizza': pizzaName,
-                'rating': rating,
-              });
-            },
-            child: Text("Dodaj"),
-          ),
-        ],
+            Slider(
+              onChanged: (newValue) {
+                setState(() {
+                  rating = newValue;
+                });
+              },
+              value: rating,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: rating.toString(),
+            ),
+            ElevatedButton(
+              onPressed: restaurantName.isEmpty || pizzaName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('restaurants').add({
+                        'name': restaurantName,
+                        'pizza': pizzaName,
+                        'rating': rating,
+                      });
+                      widget.onSave();
+                    },
+              child: Text("Dodaj"),
+            ),
+          ],
+        ),
       ),
     );
   }
