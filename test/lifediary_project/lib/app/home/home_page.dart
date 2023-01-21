29e12ab35726b262ac34ed1ee953ac4095c90cce
macieksulikowski <suleymaniana99@gmail.com>
2023-01-21 15:38:54 +0100

    Add ToDoList to app functionality

--- test/lifediary_project/lib/app/home/home_page.dart
+++ test/lifediary_project/lib/app/home/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lifediary_project/app/login/login_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,27 +21,44 @@ LoginPage moveToLogin() {
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Zalogowano!'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  //moveToLogin();
-                  //Navigator.of(context).pop();
-                  // Navigator.pushNamed(context, '/login');
-                },
-                child: const Text('Wyloguj')),
-          ],
-        ),
-      ),
+      body: Builder(builder: (context) {
+        if (currentIndex == 1) {
+          return Center(
+            child: Text('2'),
+          );
+        }
+        if (currentIndex == 2) {
+          return Center(
+            child: Text('3'),
+          );
+        }
+        if (currentIndex == 3) {
+          return Center(
+            child: Text('4'),
+          );
+        }
+        if (currentIndex == 4) {
+          return ToDoListContent();
+        }
+        if (currentIndex == 5) {
+          return Center(
+            child: Text('6'),
+          );
+        }
+        return MyAccountPageContent(email: widget.user.email);
+      }),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -68,6 +87,121 @@ class _HomePageState extends State<HomePage> {
         ],
         type: BottomNavigationBarType.fixed,
       ),
+    );
+  }
+}
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    Key? key,
+    required this.email,
+  }) : super(key: key);
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Zalogowano jako $email! '),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                //moveToLogin();
+                //Navigator.of(context).pop();
+                // Navigator.pushNamed(context, '/login');
+              },
+              child: const Text('Wyloguj')),
+        ],
+      ),
+    );
+  }
+}
+
+class ToDoListContent extends StatelessWidget {
+  ToDoListContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  final controller = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'LIFEDIARY',
+          style: GoogleFonts.lato(
+              color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance.collection('categories').add(
+            {
+              'title': controller.text,
+            },
+          );
+          controller.clear();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('categories').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Wystapil blad');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Prosze czekac, trwa ladowanie');
+            }
+
+            final documents = snapshot.data!.docs;
+            return ListView(
+              children: [
+                for (final document in documents) ...[
+                  Dismissible(
+                    key: ValueKey(document.id),
+                    onDismissed: (_) {
+                      FirebaseFirestore.instance
+                          .collection('categories')
+                          .doc(document.id)
+                          .delete();
+                    },
+                    child: CategoryWidget(
+                      document['title'],
+                    ),
+                  ),
+                ],
+                TextField(controller: controller),
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(10),
+      child: Text(title),
     );
   }
 }
