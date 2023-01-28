9669f3e62bde2e102e1d32b8a0e2e962e862f8ed
macieksulikowski <suleymaniana99@gmail.com>
2023-01-28 12:48:47 +0100

    Add DetailsPage

---
+++ test/lifediary_project/lib/app/details/pages/details_page.dart
@@ -0,0 +1,164 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lifediary_project/app/add_page/add_page.dart';
+import 'package:lifediary_project/app/details/cubit/details_cubit.dart';
+
+import 'package:lifediary_project/app/models/item_model.dart';
+import 'package:lifediary_project/app/repositories/items_repository.dart';
+
+int maxDiaryCount = 3;
+int currentDiaryCounter = 0;
+
+class DetailsPageContent extends StatelessWidget {
+  const DetailsPageContent({
+    required this.id,
+    Key? key,
+  }) : super(key: key);
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'LIFEDIARY',
+          style: GoogleFonts.lato(
+              color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: BlocProvider(
+        create: (context) => DetailsCubit(ItemsRepository())..getItemWithID(id),
+        child: BlocBuilder<DetailsCubit, DetailsState>(
+          builder: (context, state) {
+            final itemModel = state.itemModel;
+            if (itemModel == null) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return ListView(
+              children: [
+                ListViewItem(
+                  itemModel: itemModel,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return BlocProvider(
+    create: (context) => DetailsCubit(ItemsRepository()),
+    child: BlocBuilder<DetailsCubit, DetailsState>(
+      builder: (context, state) {
+        final itemModel = state.itemModel;
+        if (itemModel == null) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return ListView(
+          children: [
+            ListViewItem(
+              itemModel: itemModel,
+            ),
+          ],
+        );
+      },
+    ),
+  );
+}
+
+class ListViewItem extends StatelessWidget {
+  const ListViewItem({
+    Key? key,
+    required this.itemModel,
+  }) : super(key: key);
+
+  final ItemModel itemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 30,
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black12,
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    itemModel.imageURL,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          itemModel.title,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          itemModel.releaseDate.toString(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white70,
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(
+                        itemModel.daysLeft(),
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text('days left'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- test/lifediary_project/lib/app/home/diaries/diaries_page_content.dart
+++ test/lifediary_project/lib/app/home/diaries/diaries_page_content.dart
@@ -6,6 +6,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lifediary_project/app/add_page/add_page.dart';
+import 'package:lifediary_project/app/details/pages/details_page.dart';
 import 'package:lifediary_project/app/home/diaries/cubit/diares_cubit.dart';
 import 'package:lifediary_project/app/models/item_model.dart';
 import 'package:lifediary_project/app/repositories/items_repository.dart';
@@ -114,75 +115,84 @@ class ListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 30,
-      ),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black12,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetailsPageContent(id: itemModel.id),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 30,
         ),
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                image: DecorationImage(
-                  image: NetworkImage(
-                    itemModel.imageURL,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.black12,
+          ),
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      itemModel.imageURL,
+                    ),
+                    fit: BoxFit.cover,
                   ),
-                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            itemModel.title,
+                            style: const TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            itemModel.releaseDate.toString(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white70,
+                    ),
                     margin: const EdgeInsets.all(10),
                     padding: const EdgeInsets.all(10),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          itemModel.title,
+                          itemModel.daysLeft(),
                           style: const TextStyle(
                             fontSize: 20.0,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          itemModel.releaseDate.toString(),
-                        ),
+                        const Text('days left'),
                       ],
                     ),
                   ),
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  margin: const EdgeInsets.all(10),
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        itemModel.daysLeft(),
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text('days left'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
