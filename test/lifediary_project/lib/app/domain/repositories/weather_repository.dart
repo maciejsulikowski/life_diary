4605fe382fe157585ead11e0f6ba2d61bc6d34bd
macieksulikowski <suleymaniana99@gmail.com>
2023-06-16 11:56:47 +0200

    Adding checking temperature functionality from city

--- test/lifediary_project/lib/app/domain/repositories/weather_repository.dart
+++ test/lifediary_project/lib/app/domain/repositories/weather_repository.dart
@@ -1,13 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:lifediary_project/app/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    //http://api.weatherapi.com/v1/current.json?key=2f4cf53875c246c7b2a164521231801&q=Olsztyn&aqi=no
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=2f4cf53875c246c7b2a164521231801&q=$city&aqi=no');
+    final responseData = response.data;
+    if (responseData == null) {
+      return null;
+    }
+    final name = responseData['location']['name'] as String;
+    final temp = (responseData['current']['temp_c'] + 0.0) as double;
+
     return WeatherModel(
-      temperature: -6.5,
-      city: 'Warsaw',
+      temperature: temp,
+      city: name,
     );
   }
 }
