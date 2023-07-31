a471be1c7d96c711f3d8173c0b0b68ce69e93bfa
macieksulikowski <suleymaniana99@gmail.com>
2023-07-31 21:45:00 +0200

    ok2

--- lifediary_project/lib/main.dart
+++ lifediary_project/lib/main.dart
@@ -12,6 +12,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );//dss
   runApp(const MyApp());
 }
