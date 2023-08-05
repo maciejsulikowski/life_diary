6d4f370b9ffe3f28516c2b9fbfa76bdb3465d036
macieksulikowski <suleymaniana99@gmail.com>
2023-08-05 10:31:20 +0200

    Test commits

--- lifediary_project/lib/app/features/home/home_page.dart
+++ lifediary_project/lib/app/features/home/home_page.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
         onTap: (newIndex) {
           setState(() {
             currentIndex = newIndex;
-          });
+          });//ss
         },
         items: const [
           BottomNavigationBarItem(
