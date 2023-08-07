800f0be500353534acf090ea09d193a2e32210b0
macieksulikowski <suleymaniana99@gmail.com>
2023-08-07 22:35:32 +0200

    Add item_model_test

--- lifediary_project/test/domain/models/item_model_test.dart
+++ lifediary_project/test/domain/models/item_model_test.dart
@@ -3,17 +3,34 @@ import 'package:lifediary_project/app/domain/models/item_model.dart';
 import 'package:intl/intl.dart';
 
 void main() {
-  test('', () {
+  test('Should return releaseDateFormatted', () {
     //1
     final model = ItemModel(
         id: '1',
         title: 'title',
         imageURL: 'imageURL',
-        releaseDate: DateFormat('yMMMEd').parse('Thu, Jul 6, 2023'),
+        releaseDate: DateTime(2023, 7, 6),
         text: []);
 
     //2
     final results = model.releaseDateFormatted();
     //3
+    expect(results, 'Thu, Jul 6, 2023');
+  });
+
+  test('Should return correct number of days', () {
+    //1
+    final releaseDate = DateTime.now().add(const Duration(days: 7));
+    final model = ItemModel(
+        id: '1',
+        title: 'title',
+        imageURL: 'imageURL',
+        releaseDate: releaseDate,
+        text: []);
+
+    //2
+    final results = model.daysLeft();
+    //3
+    expect(results, '7');
   });
 }
